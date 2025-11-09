56e881fca747dc72e26c533752a885c1cae78295
emilyscherny-create <emilyscherny@gmail.com>
2025-11-09 18:45:10 -0500

    Fix CSS import path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import import './assets/styles/app.css';  // Corrected import path
+import './app.css';
 
 // This mounts the Infraread app into index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
